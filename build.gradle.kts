db67e337a61a7c0422367022c794fd8e20a0fd8e
kanchitsu <dev@kanchitsu.top>
2026-05-10 14:20:23 +0800

    fix: add JetBrains Compose repository and subprojects config

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,3 +3,14 @@ plugins {
     alias(libs.plugins.compose) apply false
     alias(libs.plugins.kotlin.compose.compiler) apply false
 }
+
+subprojects {
+    repositories {
+        mavenCentral()
+        maven { url = uri("https://repo.spring.io/milestone") }
+        maven { url = uri("https://repo.spring.io/snapshot") }
+        maven { url = uri("https://oss.sonatype.org/content/groups/public/") }
+        maven { url = uri("https://maven.aliyun.com/repository/public") }
+        maven { url = uri("https://maven.pkg.jetbrains.space/public/p/compose/dev") }
+    }
+}
