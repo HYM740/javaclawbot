cf62298ff74feb67015e7c6b8616922874ed0ec1
kanchitsu <dev@kanchitsu.top>
2026-05-10 19:55:28 +0800

    fix: 开发控制台页添加垂直滚动

--- compose-app/src/main/kotlin/gui/ui/pages/DevConsolePage.kt
+++ compose-app/src/main/kotlin/gui/ui/pages/DevConsolePage.kt
@@ -2,6 +2,8 @@ package gui.ui.pages
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -13,7 +15,9 @@ import gui.ui.theme.*
 @Composable
 fun DevConsolePage(bridge: Bridge?, modifier: Modifier = Modifier) {
     Column(
-        Modifier.fillMaxSize().background(AppColors.Background).padding(40.dp, 24.dp, 24.dp, 24.dp),
+        Modifier.fillMaxSize().background(AppColors.Background)
+            .verticalScroll(rememberScrollState())
+            .padding(40.dp, 24.dp, 24.dp, 24.dp),
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Dev Console", style = AppTheme.typography.title)
